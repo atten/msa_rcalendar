27c8698ad7c7bac5bb7b0b245fd0ea26561b5d0c
night-crawler <lilo.panic@gmail.com>
2017-12-22 12:44:20 +0300

    Bumped version: 0.0.0 → 0.0.1

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t docker.force.fm/msa/rcalendar:latest \
-             -t docker.force.fm/msa/rcalendar:0.0.0 \
+             -t docker.force.fm/msa/rcalendar:0.0.1 \
              .
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker push docker.force.fm/msa/rcalendar:latest
-docker push docker.force.fm/msa/rcalendar:0.0.0
+docker push docker.force.fm/msa/rcalendar:0.0.1
